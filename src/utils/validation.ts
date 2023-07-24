3dd964ad42c48f256b21986a1bb1c5a50595deeb
Javier Diaz <coderdiaz@gmail.com>
2023-07-24 13:01:15 -0600

    feat: added missing options on card brands

--- src/utils/validation.ts
+++ src/utils/validation.ts
@@ -1,19 +1,19 @@
 enum CardType {
-  Visa = 'visa',
-  Mastercard = 'mastercard',
   Amex = 'amex',
-  Diners = 'diners',
+  Diners = 'dinersclub',
   Discover = 'discover',
+  Jcb = 'jcb',
+  Mastercard = 'mastercard',
+  Unionpay = 'unionpay',
+  Visa = 'visa',
 }
 
 type CardTypeConfig = { size: number[]; prefixes: string[] };
 type CardTypes = { [key in CardType]: CardTypeConfig };
 
 const options: CardTypes = {
-  visa: { size: [13, 16], prefixes: ['4'] },
-  mastercard: { size: [16], prefixes: ['51', '52', '53', '54', '55'] },
   amex: { size: [15], prefixes: ['34', '37'] },
-  diners: {
+  dinersclub: {
     size: [14],
     prefixes: ['300', '301', '302', '303', '304', '305', '36', '38'],
   },
@@ -21,6 +21,43 @@ const options: CardTypes = {
     size: [16],
     prefixes: ['6011', '644', '645', '646', '647', '648', '649', '65'],
   },
+  jcb: {
+    size: [16],
+    prefixes: ['3528', '3529', '353', '354', '355', '356', '357', '358'],
+  },
+  mastercard: { size: [16], prefixes: ['51', '52', '53', '54', '55'] },
+  unionpay: {
+    size: [16, 17, 18, 19],
+    prefixes: [
+      '622126',
+      '622127',
+      '622128',
+      '622129',
+      '62213',
+      '62214',
+      '62215',
+      '62216',
+      '62217',
+      '62218',
+      '62219',
+      '6222',
+      '6223',
+      '6224',
+      '6225',
+      '6226',
+      '6227',
+      '6228',
+      '62290',
+      '62291',
+      '622920',
+      '622921',
+      '622922',
+      '622923',
+      '622924',
+      '622925',
+    ],
+  },
+  visa: { size: [13, 16], prefixes: ['4'] },
 };
 
 /**
@@ -34,13 +71,12 @@ const options: CardTypes = {
  */
 export const getCardBrand = (cardNumber: string) => {
   const formattedNumber = cardNumber.replace(/[ -]/g, ''); // Removing spaces from the card number
-  const validateCode = formattedNumber.substring(0, 4); // Getting the first four gits of the card number
 
   for (const [option, config] of Object.entries(options)) {
     const { size, prefixes } = config;
     if (size.includes(formattedNumber.length)) {
       const testCard = new RegExp(`^(${prefixes.join('|')})`);
-      if (testCard.test(validateCode)) {
+      if (testCard.test(formattedNumber)) {
         return option;
       }
     }
